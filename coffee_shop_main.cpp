73e027cd9b6b05d98528b365e990d9847c484d93
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-10-17 19:40:01 -0700

    Initial commit

---
+++ coffee_shop_main.cpp
@@ -0,0 +1,13 @@
+// COMSC-210 | Midterm #2 | Yukun Zhang
+// IDE used: Visual Studio Code
+
+//I'm used to opening a separate file to program the project, 
+//rather than with the entire code of a class, which is more readable. 
+//But since I don't have time to rewrite the header files, I'll just 
+//include the .cpp file, which still works. Make sure the 210-midterm-starter.cpp 
+//is in the same folder as the current file. And that210-midterm-starter.cppdoes not have a main.
+
+#include <iostream>
+#include "210-midterm-starter.cpp"
+using namespace std;
+
