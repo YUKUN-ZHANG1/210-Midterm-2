b1df544ced0826a7b0423d853aa00695b921b58b
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-10-17 20:01:35 -0700

    3()

--- coffee_shop_main.cpp
+++ coffee_shop_main.cpp
@@ -8,13 +8,38 @@
 //is in the same folder as the current file. And that210-midterm-starter.cppdoes not have a main.
 
 #include <iostream>
+#include <fstream>
 #include "210-midterm-starter.cpp"
 using namespace std;
 
 const string FILENNAME = "names";
 
+int createNameArray (string filename, string* names) {
+    ifstream inputFile("names.txt"); 
+    if (!inputFile) {
+        cout << "File open failure" << endl;
+        return 1;
+    }
+
+    int count = 0;
+    string name = "";
+    while (std::getline(inputFile, name)) {
+        count++;
+    }
+
+    inputFile.clear();
+    inputFile.seekg(0, ios::beg);
+
+    if
+    names = new string[count];
+    
+}
+
 
 int main() {
+    string* names;
+    int names_size = createNameArray(FILENNAME, names);
+
     return 0;
 }
 
